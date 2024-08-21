7392e3631f818d5bc10ad60935a000a67708f2b8
Avinash <ache0161@student.monash.edu>
2024-08-21 16:12:59 +1000

    completed week5

--- src/main.js
+++ src/main.js
@@ -1,22 +1,22 @@
-
 // import './style.css'
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import PrimeVue from 'primevue/config';
 import Aura from '@primevue/themes/aura';
 
-const app=createApp(App);
-app.use(PrimeVue,{
-    theme:{
-        preset:Aura
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
     }
 });
 
+app.use(router);
 
-
-
-
-createApp(App).mount('#app')
+// Mount the app only once
+app.mount('#app');
