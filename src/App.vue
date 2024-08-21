6f963ccfadb20c1d481727fe2177befbd5db05d7
Avinash <ache0161@student.monash.edu>
2024-08-21 11:30:05 +1000

    added routing

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,20 @@ import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
 import JSON from './components/JSON.vue';
 import Form from './components/Form.vue';
+import BHeader from './components/BHeader.vue';
 </script>
 
 <template>
-    <!-- <JSON /> -->
-     <Form />
+  <div class="main-container">
+    <header>
+      <BHeader />
+    </header>
+    <main class="main-box">
+      <!-- <JSON /> -->
+      <!-- <Form /> -->
+      <router-view></router-view>
+    </main>
+  </div>
 </template>
 
 <style scoped>
